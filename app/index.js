d56608613d8cc85dd1d91c385f0e2959faec8faa
hawkup <spiritsquare@gmail.com>
2016-07-31 07:11:07 +0700

    Added process.exit

--- app/index.js
+++ app/index.js
@@ -3,8 +3,10 @@ const client = redis.createClient({ url: 'redis://redis:6379' });
 
 client.on('error', (err) => {
   console.log(`Error ${err}`);
+  process.exit(1);
 });
 
 client.on('ready', () => {
   console.log('connected with redis');
+  process.exit(0);
 });
